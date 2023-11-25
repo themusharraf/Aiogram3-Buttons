824ed524925e84e3a51f9392dda1edde85a81caf
allnc <meibrohimov@gmail.com>
2023-11-25 23:31:11 +0500

    change file

---
+++ inline.py
@@ -0,0 +1,12 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+async def cmd_inline_url(message: types.Message, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="GitHub", url="https://github.com")
+    )
+    builder.row(types.InlineKeyboardButton(
+        text="Оф. канал Telegram",
+        url="tg://resolve?domain=telegram")
+    )
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import asyncio
 import logging
-
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
 from config import token
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from keyboards import keyboard
 
 bot = Bot(token=token)
 dp = Dispatcher()
@@ -18,7 +16,12 @@ async def cmd_start(message: types.Message):
 
 @dp.message(Command("buttons"))
 async def buttons(message: types.Message):
-    ...
+    await message.answer("location or contact", reply_markup=keyboard)
+
+
+@dp.message(Command("in_url"))
+async def button(message: types.Message):
+    await message.answer("inline button!")
 
 
 async def main():
